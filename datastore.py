dcb6f812a891eb0ce2cdd2df47ecba27bf640aa9
Katie Picha <Katie_Picha1@baylor.edu>
2021-09-08 16:39:23 -0500

    new changes

--- datastore.py
+++ datastore.py
@@ -57,4 +57,7 @@ outfile = open("retail_store.csv", 'w')
 outfile.write('room-number,use,sq-ft,price\n')
 
 for l in datastore['medical']:
-  outfile.write(str(l["room-number"]) + ',' + l['use'] + ',' + str(l["sq-ft"]) + ',' + str(l["price"]) + '\n'
+  outfile.write(str(l["room-number"]) + ',' + l['use'] + ',' + str(l["sq-ft"]) + ',' + str(l["price"]) + '\n')
+# str method must be used because integers and floats must be converted to a string in order to write a file
+
+outfile.close()
