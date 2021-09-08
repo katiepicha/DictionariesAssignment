2f10360d5ff5c06bedb6bc1b0da30698e510558a
Katie Picha <Katie_Picha1@baylor.edu>
2021-09-08 17:18:43 -0500

    new changes

--- in class exercise.py
+++ in class exercise.py
@@ -15,5 +15,35 @@ def main():
 # Call the main function.
 main()
 
-outfile = open("philosophers.txt", 'a')
-outfile.write('\nKatie Picha')
+
+# add your name to the bottom of the list (append)
+def main1():
+
+    outfile = open("philosophers.txt", 'a')
+
+    outfile.write('\nKatie Picha')
+
+    outfile.close()
+
+main1()
+
+
+# reading from the philosophers file
+
+def main2():
+
+    infile = open("philosophers.txt", 'r')
+
+    # read the file's contents
+    file_contents = infile.read()
+
+    line1 = infile.readline().rstrip('\n')
+    line2 = infile.readline()
+    line3 = infile.readline()
+
+    print(line1)
+    print(line2.rstrip('\n'))
+    print(line3)
+
+
+main2()
